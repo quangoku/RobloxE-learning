f5100e701c2e50a1f03979eaab59a49e09a9c470
quangoku <lequan159753@gmail.com>
2025-10-17 21:09:46 +0700

    fix upload avatar

--- app/api/user/uploadAvatar/route.ts
+++ app/api/user/uploadAvatar/route.ts
@@ -3,6 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../../auth/[...nextauth]/route";
+import type { UploadApiResponse } from "cloudinary";
 
 export async function POST(req: NextRequest) {
   const session = await getServerSession(authOptions);
@@ -11,17 +12,17 @@ export async function POST(req: NextRequest) {
     const formData = await req.formData();
     const file = formData.get("file");
 
-    if (!file) {
+    if (!file || !(file instanceof File)) {
       return NextResponse.json({ message: "File not found" }, { status: 404 });
     }
 
     const bytes = await file.arrayBuffer();
     const buffer = Buffer.from(bytes);
 
-    const uploadRespone = await new Promise((res, rej) => {
+    const uploadRespone: UploadApiResponse = await new Promise((res, rej) => {
       cloudinary.uploader
         .upload_stream((err, result) => {
-          if (err) rej(err);
+          if (err || !result) rej(err);
           else res(result);
         })
         .end(buffer);
@@ -44,7 +45,7 @@ export async function POST(req: NextRequest) {
         status: 200,
       }
     );
-  } catch (error) {
+  } catch {
     return NextResponse.json(
       {
         message: "Failed to upload img",
